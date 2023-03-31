9ad301205e12aaf4a88282e8fbc90bc2ec2e7b64
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-03-31 14:28:05 +0430

    Fourth

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,64 +1,60 @@
 package ir.ac.kntu;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.lang.reflect.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int line_count=1;
-        int public_class_arrived=0;
-        int public_class_main=0;
+        int line_count = 1;
+        int public_class_arrived = 0;
+        int public_class_main = 0;
         Scanner scanner = new Scanner(System.in);
         String fileName = scanner.nextLine();
         BufferedReader reader;
+
         try {
             reader = new BufferedReader(new FileReader("src/main/java/ir/ac/kntu/" + fileName));
             String line = reader.readLine();
 
             while (line != null) {
-                if(line.length()>80)
-                {
-                    System.out.printf("line.length in line %d\n",line_count);
-                }
-                if(line.startsWith("package ") && line_count!=1)
-                {
-                     System.out.printf("package_error in line %d\n",line_count);
-                }
-                if(line.startsWith("import ") && public_class_arrived!=0)
-                {
-                    System.out.printf("import_error in line %d\n",line_count);
-                }
-                if(line.startsWith("public class "))
-                {
-                    public_class_arrived=1;
-                    if(!line.matches("public class ^[A-Z][a-zA-Z0-9]*$"))
-                    {
-                        System.out.printf("classname_error in line %d\n",line_count);
+                check_var(line,line_count);
+                int count = 0;
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ';') {
+                        count++;
                     }
                 }
-                if(line.startsWith("    public static void main"))
-                {
-                    public_class_main=1;
+                if (count > 1) {
+                    System.out.printf("semicolon error in line %d\n", line_count);
                 }
-                if(public_class_main==1)
-                {
-                    int count=0;
-                    for(int i=0;i<line.length();i++)
-                    {
-                        if(line.charAt(i)==';')
-                        {
-                            count++;
-                        }
-                    }
-                    if(count>1)
-                    {
-                        System.out.printf("semi-column error in line %d\n",line_count);
+                if (line.length() > 80) {
+                    System.out.printf("line.length in line %d\n", line_count);
+                }
+                if (line.startsWith("package ") && line_count != 1) {
+                    System.out.printf("package_error in line %d\n", line_count);
+                }
+                if (line.startsWith("import ") && public_class_arrived != 0) {
+                    System.out.printf("import_error in line %d\n", line_count);
+                }
+                if (line.startsWith("public class ")) {
+                    public_class_arrived = 1;
+                    if (!line.matches("public class [A-Z][a-zA-Z].*")) {
+                        System.out.printf("classname_error in line %d\n", line_count);
                     }
                 }
-                
+                if (line.startsWith("    public static void main")) {
+                    public_class_main = 1;
+                }
+                if (line.startsWith("    public static ")) {
+                    check_met(line, line_count);
+                }
                 line = reader.readLine();
                 line_count++;
             }
@@ -68,4 +64,27 @@ public class Main {
         }
     }
 
+    public static void check_met(String line, int line_N) {
+        int openParenIndex = line.indexOf("(");
+        int lastSpaceIndex = line.lastIndexOf(" ", openParenIndex);
+        String methodName = line.substring(lastSpaceIndex + 1, openParenIndex);
+        if (!methodName.matches("^[a-z]+([A-Z][a-z]*)*$")) {
+            System.out.println("methoderror method:  " + methodName + " in Line: " + line_N);
+        }
+    }
+
+    public static void check_var(String line, int line_N) {
+        String pattern = "(\\b[a-zA-Z_]\\W*)\\s*=.*";
+
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(line);
+        if(m.find())
+        {
+            String variableName = m.group(1);
+            if(!variableName.matches("^[a-z]+([A-Z][a-z]*)*$"))
+            {
+                System.out.println("variable_error in line :" + line_N);
+            }
+        }
+    }
 }
--- src/main/java/ir/ac/kntu/Test.java
+++ src/main/java/ir/ac/kntu/Test.java
@@ -2,6 +2,26 @@ package ir.ac.kntu;
 
 public class Test {
     public static void main(String[] args) {
-       System.out.println("jjdsfl");
+        System.out.println("jjdsfl");
+        int jdkj=9;
+    
+    }
+
+    public static void tabee(String review) {
+        // TODO
+    }
+
+    public static void LowerCamelCase(double score) {
+        // TODO
+        boolean LowerCamelCase;
+    }
+
+    public static int getId() {
+        int x=8;
+        return 5;
+        
+    }
+    public static int uhwfl() {
+        return 5;
     }
 }
